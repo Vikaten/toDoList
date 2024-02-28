e8e597bc9baf972918a68c3684a2f9f2a1365b99
teiltor <vikaten06@gmail.com>
2024-02-28 22:56:30 +0300

    попытка установить checked для checkbox

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const heading = document.querySelector("#top");
 const space = document.querySelector("#space");
 const noticeButton = document.querySelector("#noticeButton");
 const ul = document.querySelector("ul");
+let container = document.querySelector("#container");
 let toDoList = [];
 
 if (localStorage.getItem("todo")) {
@@ -9,10 +10,11 @@ if (localStorage.getItem("todo")) {
     createNewDoing();
 } 
 
+// проверка ну пустую строку в графе
 noticeButton.addEventListener("click", function () {
     let toDo = {
     doing: space.value,
-    checkbox: false,
+    checkbox: false
     };
     if (toDo.doing.length > 0) {
       toDoList.push(toDo);
@@ -21,7 +23,7 @@ noticeButton.addEventListener("click", function () {
     else {
       alert("Ваша строка должна пустая. Заполните ее, пожалуйста!");
     }
-  console.log(toDoList);
+  // console.log(toDoList);
   localStorage.setItem("todo", JSON.stringify(toDoList));
 });
 
@@ -32,11 +34,25 @@ function createNewDoing() {
     pr += `
         <li id = 'item${index}'>
         <div class = 'checkText'>
-            <input type = 'checkbox' class = 'checkbox'>
-            <label class = 'li'>${el.doing}</label>
+            <input type = 'checkbox' class = 'checkbox' id = 'item${index}'>
+            <label for = 'item${index}'>${el.doing}</label>
         </div>
         </li>`;
     space.value = "";
     ul.innerHTML = pr;
   });
 }
+
+// сохранение состояния checkbox
+container.addEventListener('change', function (e) {
+  let targetTag = e.target.getAttribute('id');
+  toDoList.forEach((el, index) => {
+    if (el.id === targetTag) {
+      el.checkbox = !el.checkbox;
+      localStorage.setItem("todo", JSON.stringify(toDoList));
+    }
+    // else {
+    //   console.log(`item${index}` + ' ' + targetTag)
+    // }
+  });
+});
