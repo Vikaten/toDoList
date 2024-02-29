7ef89e49f7621423c2157132a7d7e59d7adffc09
teiltor <vikaten06@gmail.com>
2024-02-29 22:16:41 +0300

    конечная установка checked, чтобы при обновлении оставалась галочка

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const heading = document.querySelector("#top");
 const space = document.querySelector("#space");
 const noticeButton = document.querySelector("#noticeButton");
 const ul = document.querySelector("ul");
-let container = document.querySelector("#container");
+const container = document.querySelector("#container");
 let toDoList = [];
 
 if (localStorage.getItem("todo")) {
@@ -13,8 +13,8 @@ if (localStorage.getItem("todo")) {
 // проверка ну пустую строку в графе
 noticeButton.addEventListener("click", function () {
     let toDo = {
-    doing: space.value,
-    checkbox: false
+      doing: space.value,
+      checkbox: false
     };
     if (toDo.doing.length > 0) {
       toDoList.push(toDo);
@@ -23,7 +23,6 @@ noticeButton.addEventListener("click", function () {
     else {
       alert("Ваша строка должна пустая. Заполните ее, пожалуйста!");
     }
-  // console.log(toDoList);
   localStorage.setItem("todo", JSON.stringify(toDoList));
 });
 
@@ -34,7 +33,7 @@ function createNewDoing() {
     pr += `
         <li id = 'item${index}'>
         <div class = 'checkText'>
-            <input type = 'checkbox' class = 'checkbox' id = 'item${index}'>
+            <input type = 'checkbox' class = 'checkbox' id = 'item${index}' ${el.checkbox ? 'checked' : ''}>
             <label for = 'item${index}'>${el.doing}</label>
         </div>
         </li>`;
@@ -47,12 +46,9 @@ function createNewDoing() {
 container.addEventListener('change', function (e) {
   let targetTag = e.target.getAttribute('id');
   toDoList.forEach((el, index) => {
-    if (el.id === targetTag) {
-      el.checkbox = !el.checkbox;
+    if ('item' + toDoList.indexOf(el) == targetTag) {
+    el.checkbox = !el.checkbox;
       localStorage.setItem("todo", JSON.stringify(toDoList));
     }
-    // else {
-    //   console.log(`item${index}` + ' ' + targetTag)
-    // }
-  });
-});
+  })
+})
